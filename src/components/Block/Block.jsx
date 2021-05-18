786f36205ec63a987f5fe98f0c7975908a3caf34
Oigen43 <oigen43@icloud.com>
2021-05-18 12:49:23 +0300

    add env

--- src/components/Block/Block.jsx
+++ src/components/Block/Block.jsx
@@ -3,7 +3,7 @@ import { memo } from 'react';
 import styles from './Block.module.scss';
 
 function Block() {
-  return <div className={styles.container}>AAAA</div>
+  return <div className={styles.container}>AAAA</div>;
 }
 
 export default memo(Block);
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,21 @@
 import Block from 'components/Block';
 
 export default function Home() {
+  console.log(process.env.CHECK);
+  console.log(process.env.NEXT_PUBLIC_CHECK);
+
   return (
     <>
       <h1>AAA</h1>
       <Block />
     </>
   );
+}
+
+export async function getStaticProps() {
+  console.log('build');
+  console.log(process.env.CHECK);
+  console.log(process.env.NEXT_PUBLIC_CHECK);
+
+  return { props: { check: 1 } };
 }
